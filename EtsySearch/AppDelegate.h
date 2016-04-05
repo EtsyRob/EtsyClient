1a8185b665926a5742fdac371d6889d7c81dfec0
Rob Day <Robert.day@dowjones.com>
2016-04-05 17:33:19 -0400

    Initial Commit

---
+++ EtsySearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EtsySearch
+//
+//  Created by Robert Day on 4/5/16.
+//  Copyright © 2016 Robert Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EtsySearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EtsySearch
+//
+//  Created by Robert Day on 4/5/16.
+//  Copyright © 2016 Robert Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
